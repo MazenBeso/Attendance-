f74fcf96cd0f1952270b04a95b7c97e7266521b2
Super Beso <124138194+MazenBeso@users.noreply.github.com>
2024-02-28 20:46:26 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function validateNumber(input) {
+function validateNumberSna3a(input) {
     var inputValue = input.value;
     inputValue = inputValue.replace(/\D/g, '');
     if (!/^\d{7}$/.test(inputValue)) {
@@ -13,7 +13,7 @@ function validateNumber(input) {
 
 function validateNumberElom(input) {
     var inputValue = input.value;
-    inputValue = inputValue.replace(/\D/g, '');
+    inputValue = inputValue;
     if (!/^\d{6}$/.test(inputValue)) {
         document.getElementById("errorText").textContent = "Type Only 6 True Numbers";
         input.setCustomValidity("Type Only 6 True Numbers");
@@ -23,6 +23,28 @@ function validateNumberElom(input) {
     }
     input.value = inputValue;
 }
+
+function validateNumberLectureRoom(input) {
+    var inputValue = input.value;
+    var sanitizedValue = '';
+    
+    for (var i = 0; i < inputValue.length; i++) {
+        if (!isNaN(inputValue[i]) && inputValue[i] !== ' ') {
+            sanitizedValue += inputValue[i];
+        }
+    }
+    
+    if (sanitizedValue.length !== 4 || isNaN(sanitizedValue)) {
+        document.getElementById("errorText").textContent = "Enter number of the room 4 digits";
+        input.setCustomValidity("Enter number of the room 4 digits");
+    } else {
+        document.getElementById("errorText").textContent = "";
+        input.setCustomValidity("");
+    }
+    
+    input.value = sanitizedValue;
+}
+
 
 function closePage() {
     window.close();
